52f6979eb9fde8bf41f2c2c4f2eb3afb48d0b051
Mikael Jorhult <mikael@jorhult.se>
2014-07-26 21:59:29 +0200

    Swedish translation.

---
+++ nls/sv/Strings.js
@@ -0,0 +1,14 @@
+define( {
+	// EXTENSION.
+	EXTENSION_NAME: "PHP Lint Tools",
+	
+	// GENERIC.
+	OK:     "OK",
+	CANCEL: "Avbryt",
+	RESET:  "Återställ",
+	
+	// SETTINGS DIALOG
+	SETTINGS_DIALOG_TITLE:     'Inställningar för PHP Lint Tools',
+	SETTINGS_DIALOG_ENABLE:    'Aktivera',
+	SETTINGS_DIALOG_STANDARDS: 'Kodstandarder'
+} );
