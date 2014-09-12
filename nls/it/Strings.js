03d3e6ef3e2f25467a7d9cc445bf6ba6d1fc1229
Christopher Pecoraro <cpecoraro@bitbucket.org>
2014-09-12 12:31:52 +0200

    Added Italian

---
+++ nls/it/Strings.js
@@ -0,0 +1,15 @@
+define( {
+	// EXTENSION.
+	EXTENSION_NAME: "PHP Code Quality Tools",
+	
+	// GENERIC.
+	OK:     "OK",
+	CANCEL: "Anulla",
+	RESET:  "Azzera",
+	
+	// SETTINGS DIALOG
+	SETTINGS_DIALOG_TITLE:            'PHP Code Quality Tools Settings',
+	SETTINGS_DIALOG_ENABLE:           'Abilita',
+	SETTINGS_DIALOG_STANDARDS:        'Standard di coding',
+	SETTINGS_DIALOG_CUSTOM_STANDARDS: 'Standard di coding personizzati',
+	SETTINGS_DIALOG_NOT_AVAILABLE:    'PHP non è disponibile sulla tua macchina.  Le estenzioni non saranno in grado di eseguire qualsiasi strumento.'} );
