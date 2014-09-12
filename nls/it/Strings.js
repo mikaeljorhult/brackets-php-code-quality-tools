7ecd35073d5c90bb43efbe9a8e37caec4c166999
Christopher Pecoraro <cpecoraro@bitbucket.org>
2014-09-12 13:33:57 +0200

    italian

--- nls/it/Strings.js
+++ nls/it/Strings.js
@@ -8,8 +8,8 @@ define( {
 	RESET:  "Azzera",
 	
 	// SETTINGS DIALOG
-	SETTINGS_DIALOG_TITLE:            'PHP Code Quality Tools Settings',
+	SETTINGS_DIALOG_TITLE:            'Impostazioni degli strumenti di qualità nella scrittura del codice.',
 	SETTINGS_DIALOG_ENABLE:           'Abilita',
-	SETTINGS_DIALOG_STANDARDS:        'Standard di scrittura del codice',
-	SETTINGS_DIALOG_CUSTOM_STANDARDS: 'Standard di scrittura del codice personalizzati',
-	SETTINGS_DIALOG_NOT_AVAILABLE:    'PHP non è disponibile sulla tua macchina.  Le estenzioni non saranno in grado di eseguire qualsiasi strumento.'} );
+	SETTINGS_DIALOG_STANDARDS:        'Standard del scrittura del codice',
+	SETTINGS_DIALOG_CUSTOM_STANDARDS: 'Standard del scrittura del codice personalizzati',
+	SETTINGS_DIALOG_NOT_AVAILABLE:    'PHP non è disponibile sulla tua macchina.  Le estenzioni non saranno in grado di eseguire qualsiasi strumento.' } );
