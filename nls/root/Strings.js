6e48a569be337ec445d6f98feeb11c608c826ccf
Mikael Jorhult <mikael@jorhult.se>
2016-06-05 13:39:31 +0200

    Fix swedish translation errors for PHP path settings.

--- nls/root/Strings.js
+++ nls/root/Strings.js
@@ -8,11 +8,11 @@ define( {
 	RESET:  "Reset",
 	
 	// SETTINGS DIALOG
-	SETTINGS_DIALOG_TITLE:            'PHP Code Quality Tools Settings',
-	SETTINGS_DIALOG_ENABLE:           'Enable',
-	SETTINGS_DIALOG_STANDARDS:        'Coding standards',
-	SETTINGS_DIALOG_CUSTOM_STANDARDS: 'Custom coding standards',
-	SETTINGS_DIALOG_NOT_AVAILABLE:    'PHP is not available on your machine. The extensions will not be able to run any tools.',
+	SETTINGS_DIALOG_TITLE:             'PHP Code Quality Tools Settings',
+	SETTINGS_DIALOG_ENABLE:            'Enable',
+	SETTINGS_DIALOG_STANDARDS:         'Coding standards',
+	SETTINGS_DIALOG_CUSTOM_STANDARDS:  'Custom coding standards',
+	SETTINGS_DIALOG_NOT_AVAILABLE:     'PHP is not available on your machine. The extensions will not be able to run any tools.',
 	SETTINGS_DIALOG_PHPLOCATION_TITLE: 'PHP Path',
 	SETTINGS_DIALOG_PHPLOCATION_INFO:  'Leave it blank to use default PHP location.',
 	SETTINGS_DIALOG_PHPLOCATION_ERROR: 'The PHP path you set is invalid.'
--- nls/sv/Strings.js
+++ nls/sv/Strings.js
@@ -8,12 +8,12 @@ define( {
 	RESET:  "Återställ",
 	
 	// SETTINGS DIALOG
-	SETTINGS_DIALOG_TITLE:            'Inställningar för PHP Code Quality Tools',
-	SETTINGS_DIALOG_ENABLE:           'Aktivera',
-	SETTINGS_DIALOG_STANDARDS:        'Kodstandarder',
-	SETTINGS_DIALOG_CUSTOM_STANDARDS: 'Anpassade kodstandarder',
-	SETTINGS_DIALOG_NOT_AVAILABLE:    'PHP är inte tillgängligt på din maskin. Tillägget kommer inte kunna köra några verktyg.',
-	SETTINGS_DIALOG_PHPLOCATION_TITLE: 'PHP Path',
-	SETTINGS_DIALOG_PHPLOCATION_INFO:  'Lämna det tomt för att använda standard PHP plats.',
-	SETTINGS_DIALOG_PHPLOCATION_ERROR: 'PHP väg du in är ogiltig.'
+	SETTINGS_DIALOG_TITLE:             'Inställningar för PHP Code Quality Tools',
+	SETTINGS_DIALOG_ENABLE:            'Aktivera',
+	SETTINGS_DIALOG_STANDARDS:         'Kodstandarder',
+	SETTINGS_DIALOG_CUSTOM_STANDARDS:  'Anpassade kodstandarder',
+	SETTINGS_DIALOG_NOT_AVAILABLE:     'PHP är inte tillgängligt på din maskin. Tillägget kommer inte kunna köra några verktyg.',
+	SETTINGS_DIALOG_PHPLOCATION_TITLE: 'PHP-sökväg',
+	SETTINGS_DIALOG_PHPLOCATION_INFO:  'Lämna fältet tomt för att använda standardsökvägen till PHP.',
+	SETTINGS_DIALOG_PHPLOCATION_ERROR: 'Den sökväg du angett är ogiltig.'
 } );
